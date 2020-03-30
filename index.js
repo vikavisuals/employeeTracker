4ab83aa81fe875fb89610fe2a809d622a6b3e78c
vkaszuba <codeword.vika@gmail.com>
2020-03-30 14:35:59 -0700

    Removal of extra files

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var mysql = require("./js/mysql.js");
-var inquirer = require("./js/inquirer.js");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employeeTracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
--- js/inquirer.js
+++
@@ -1,2 +0,0 @@
-var inquirer = require("inquirer");
-
