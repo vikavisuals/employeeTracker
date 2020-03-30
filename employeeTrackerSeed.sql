0fc29d49d6a732bce5280bc96cb197d35d701af9
vkaszuba <codeword.vika@gmail.com>
2020-03-30 14:19:46 -0700

    Setting up SQL file

---
+++ employeeTrackerSeed.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ index.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employeeTracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  createProduct();
+});
